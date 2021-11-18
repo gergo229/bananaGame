4e0dcd13b07de3f0d7b4221bb05a64fa98411f59
gergo229 <mergergo@gmial.com>
2021-11-18 16:47:59 +0100

    started the displayData struct, not finished

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include "config/config.h"
+#include "display_data/displayData.h"
+#include "string.h"
+#include "segmentlcd.h"
 
 int main(void) {
 
 	configAll();
+	struct DisplayData data;
 
+	strcpy(data.text.text, "alma");
+	data.text.isActive = 1;
+	displayData(&data);
+	SegmentLCD_Number(6);
 	while(1) {
-
 
 	}
 
