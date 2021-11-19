85476fa01dda4b7625267034f654371b5a0e3faa
gergo229 <mergergo@gmial.com>
2021-11-19 16:19:29 +0100

    fixing the global input variable problem

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
-#include "config/config.h"
+#include "config/config.h"	//for eternal configuration
+#include "input/input_handler/inputHandler.h"	//for handling the inputs
+
+volatile struct InputITFlags inputITFlags;	//a global structure, to indicate changing in inputs
 
 int main(void) {
 
