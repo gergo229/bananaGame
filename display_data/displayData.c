2d52951f2246fd8a2e3cf47c157184b7495d0699
MateSzecsi <mat.szecsi@gmail.com>
2021-11-20 19:03:39 +0100

    testing, consturctor needs to be made for displaydata

--- display_data/displayData.c
+++ display_data/displayData.c
@@ -15,24 +15,36 @@ void displayData(const struct DisplayData* const displayData){
 		SegmentLCD_Number((uint16_t) (displayData->points.max) * 100 + (uint16_t)(displayData->points.current));
 	}															// convert max and current point into one 4 digit number and print to lcd
 	if(displayData->difficulty.isActive){
-		SegmentLCD_ARing(displayData->difficulty.difficulty, 1);
+		for(int i = 0; i < displayData->difficulty.difficulty; i++)
+			SegmentLCD_ARing(i, 1);
 	}
 
 	if(displayData->gamePlay.isActive){
-		SegmentLCD_LowerCharSegments_TypeDef lowerCharSegments[7];								//initialize variable --
-		lowerCharSegments[displayData->gamePlay.bucketPosLower].BUCKET_POS_LOWER = 0;			//-- based on position of buckets --
-		lowerCharSegments[displayData->gamePlay.bucketPosUpper].BUCKET_POS_UPPER_LEFT_SEGMENT = 0;
-		lowerCharSegments[displayData->gamePlay.bucketPosUpper].BUCKET_POS_UPPER_RIGHT_SEGMENT = 0;
+		SegmentLCD_LowerCharSegments_TypeDef lowerCharSegments[7];
+		for(int i = 0; i < BANANA_MATRIX_WIDTH; i++){											//-- and position of bananas
+			lowerCharSegments[i].raw = 0;
+		}				//initialize variable --
+		lowerCharSegments[displayData->gamePlay.bucketPosLower].BUCKET_POS_LOWER = 1;			//-- based on position of buckets --
+		lowerCharSegments[displayData->gamePlay.bucketPosUpper].BUCKET_POS_UPPER_LEFT_SEGMENT = 1;
+		lowerCharSegments[displayData->gamePlay.bucketPosUpper].BUCKET_POS_UPPER_RIGHT_SEGMENT = 1;
 		for(int i = 0; i < BANANA_MATRIX_WIDTH; i++){											//-- and position of bananas
 			if(displayData->gamePlay.bananaMatrix[0][i] == 1)
-				lowerCharSegments[i].BANANA_POS_TOP = 0;
+				lowerCharSegments[i].BANANA_POS_TOP = 1;
 			if(displayData->gamePlay.bananaMatrix[1][i] == 1)
-				lowerCharSegments[i].BANANA_POS_MIDDLE = 0;
+				lowerCharSegments[i].BANANA_POS_MIDDLE = 1;
 			if(displayData->gamePlay.bananaMatrix[2][i] == 1)
-				lowerCharSegments[i].BANANA_POS_BOTTOM = 0;
+				lowerCharSegments[i].BANANA_POS_BOTTOM = 1;
 		}
 		SegmentLCD_LowerSegments(&lowerCharSegments);			//update lower segments of lcd with the initialized variable
 	}
 }
 
+void initializeDisplayData(struct DisplayData* const displayData){
+	struct DisplayData d= {.difficulty.difficulty = 0,.difficulty.isActive = 0, .points.max = 0, .points.current = 0, .points.isActive = 0, \
+			.text.text = "", .text.isActive = 0, .gamePlay.bucketPosLower = 0, .gamePlay.bucketPosUpper = 0,  .gamePlay.isActive = 0};
+	for(int i = 0; i < BANANA_MATRIX_HEIGHT; i++)
+		for(int l = 0; l < BANANA_MATRIX_WIDTH; l++)
+			d.gamePlay.bananaMatrix[i][l] = 0;
+	*displayData = d;
+}
 
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "config/config.h"	//for eternal configuration
 #include "input/input_handler/inputHandler.h"	//for handling the inputs
+#include "segmentlcd.h"
+#include "display_data/displayData.h"
 
 volatile struct InputITFlags inputITFlags;	//a global structure, to indicate changing in inputs
 
@@ -7,8 +9,25 @@ int main(void) {
 
 	configAll();
 
-	while(1) {
+	SegmentLCD_Init(false);
+	struct DisplayData displaydata;
 
+	initializeDisplayData(&displayData);
+	displaydata.difficulty.difficulty = 5;
+	displaydata.difficulty.isActive = 1;
+	displaydata.points.current = 10;
+	displaydata.points.max = 11;
+	displaydata.points.isActive = 1;
+	char alma[7] = "alamspi";
+	strcpy(displaydata.text.text, alma);
+	displaydata.text.isActive = 0;
+	displaydata.gamePlay.bucketPosUpper = 1;
+	displaydata.gamePlay.bananaMatrix[1][3]=1;
+	displaydata.gamePlay.isActive = 1;
+
+	displayData(&displaydata);
+
+	while(1) {
 
 
 	}
